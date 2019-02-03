6987532079c57e4c1c0c9977fa7efacf70c6547b
Rukayat Sadiq <fruqoyya@gmail.com>
2019-02-02 22:42:16 -0500

    set-up database

---
+++ src/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const models = require('./models')
+
+const environment = process.env.NODE_ENV
+
+const app = express()
+
+if (environment === 'development') {
+  app.use(logger('dev'))
+}
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Fetch port value from env var or use default
+const port = process.env.PORT || 8080;
+
+// Connect server to DB via sequelize
+models.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.info(`App started on port ${port} on ${environment} environment`)
+  })
+})
+
+module.exports = app
